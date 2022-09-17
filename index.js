4bb379f5f7d0657e290da662da77c09927e6a255
Arnaud293 <arnaudlt293@gmail.com>
2022-09-17 15:35:25 +0200

    Add eventListener to follow mouseMouse

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 // Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
 
+
+
+
+window.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+
+    cursor2.style.left = e.pageX + "px";
+    cursor2.style.top = e.pageY + "px";
+
+    cursor3.style.left = e.pageX + "px";
+    cursor3.style.top = e.pageY + "px";
+})
+
+
 // Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
 
 // S'assurer que les liens sont clickables
