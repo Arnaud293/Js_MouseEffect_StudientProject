94016b59d68defff5f1fb633d8db9e1699faf614
Arnaud293 <arnaudlt293@gmail.com>
2022-09-18 14:17:06 +0200

    Add simple mouseEvent (down/up)

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
-
-
-
-
 window.addEventListener('mousemove', (e) => {
     cursor.style.left = e.pageX + "px";
     cursor.style.top = e.pageY + "px";
 
     cursor2.style.left = e.pageX + "px";
     cursor2.style.top = e.pageY + "px";
-
-    cursor3.style.left = e.pageX + "px";
-    cursor3.style.top = e.pageY + "px";
 })
 
+window.addEventListener('mousedown', () => {
+    cursor.style.transform = 'scale(2) translate(-25%, -25%)';
+    cursor2.style.display='none'
+})
 
-// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
-
-// S'assurer que les liens sont clickables
-
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+window.addEventListener('mouseup', () => {
+    cursor.style.transform = 'scale(1) translate(-50%, -50%)';
+    cursor2.style.display = ''
+})
